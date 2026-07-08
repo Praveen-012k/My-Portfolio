2ff073d7516253deac97d7851a7f847536a455b2
Praveen-012k <praveenbcom48@gmail.com>
2026-07-09 02:27:21 +0530

    Initial commit: Flask portfolio site

---
+++ app.py
@@ -0,0 +1,61 @@
+from Tools.demo.mcast import sender
+from flask import Flask ,render_template , request , redirect ,url_for,flash
+from dotenv import load_dotenv
+import os
+from smtplib import SMTP
+
+
+from pyexpat.errors import messages
+
+load_dotenv()
+
+password = os.environ.get("PASSWORD")
+my_email = os.environ.get("MY_EMAIL")
+sender_email = os.environ.get("sending_email")
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+@app.route("/")
+def home():
+
+
+
+
+
+    return render_template("index.html")
+
+
+@app.route("/submit",methods=["GET","POST"])
+def submit():
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        try:
+            with SMTP("smtp.gmail.com",port=587) as connection:
+                connection.starttls()
+                connection.login(sender_email,password)
+                connection.sendmail(from_addr=sender_email,
+                                    to_addrs=my_email
+                                    ,msg=f"Subject:From portfolio contacts\n\n Name : {name}\n"
+                                         f"Email : {email}\n"
+                                         f"Message : {message}")
+
+
+                flash("Mail sent Successfully")
+
+                return redirect(url_for("home"))
+
+        except Exception as e:
+            print(e)
+            print("Something went wrong try again")
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
